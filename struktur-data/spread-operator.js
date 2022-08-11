ed4cfb76da529dc4cd776700206da7bd54be4226
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-08-11 14:06:12 +0700

    Belajar Spread Operator✨

---
+++ struktur-data/spread-operator.js
@@ -0,0 +1,17 @@
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+
+console.log(favorites);
+console.log(...favorites);
+
+const others = ["Cake", "Pie", "Donut"];
+
+const allFavorites = [...favorites, ...others];
+
+console.log(allFavorites);
+
+const obj1 = { firstName: 'Obi-Wan', age: 30 };
+const obj2 = { lastName: 'Kenobi', gender: 'M' };
+
+const newObj = { ...obj1, ...obj2 };
+
+console.log(newObj);
