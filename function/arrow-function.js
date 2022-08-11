e63d5d68bae827f73daa07900bdbb096f5bb6a87
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-08-11 20:55:55 +0700

    Belajar Arrow Function✨

---
+++ function/arrow-function.js
@@ -0,0 +1,36 @@
+// regular function
+
+// function declaration
+// function sayHello(greet) {
+//     console.log(`${greet}!`);
+// }
+
+// function expression
+// const sayName = function (name) {
+//     console.log(`Nama saya ${name}`)
+// }
+
+// arrow function
+
+// function expression
+// const sayHello = (greet) => {
+//     console.log(`${greet}!`)
+// }
+
+// const sayName = (name) => {
+//     console.log(`Nama saya ${name}`)
+// }
+
+// sayName("Leia");
+
+// const sayHello = () => {
+//     console.log("Selamat pagi semuanya!")
+// };
+
+// sayHello();
+
+const sayName = name => console.log(`Nama saya ${name}`);
+sayName("Leia");
+
+const sayHello = () => console.log("Selamat pagi semuanya!");
+sayHello();
