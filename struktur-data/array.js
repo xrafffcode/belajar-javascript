91b5236d5582198c46b1c71a31c1c78565e73cc1
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-08-11 14:06:24 +0700

    Belajar Array✨

---
+++ struktur-data/array.js
@@ -0,0 +1,42 @@
+// let myArray = ["Cokelat", 42.5, 22, true, "Programming"];
+// console.log(myArray);
+
+// let myArray = ["Cokelat", 42.5, 22, true, "Programming"];
+// console.log(myArray[1]);
+
+// let myArray = ["Cokelat", 42.5, 22, true, "Programming"];
+// console.log(myArray[0]);
+// console.log(myArray[1]);
+// console.log(myArray[2]);
+// console.log(myArray[3]);
+// console.log(myArray[4]);
+// console.log(myArray[5]);
+// console.log("Panjang nilai myArray adalah " + myArray.length + ".");
+
+
+// const myArray = ["Cokelat", 42.5, 22, true, "Programming"];
+
+// myArray.push('JavaScript');
+// console.log(myArray);
+
+// const myArray = ["Cokelat", 42.5, 22, true, "Programming"];
+
+// myArray.pop();
+// console.log(myArray);
+
+// const myArray = ["Cokelat", 42.5, 22, true, "Programming"];
+
+// myArray.shift();
+// myArray.unshift("Apple");
+
+// console.log(myArray);
+
+// const myArray = ["Cokelat", 42.5, 22, true, "Programming"];
+
+// delete myArray[1];
+// console.log(myArray);
+
+// const myArray = ["Cokelat", 42.5, 22, true, "Programming"];
+
+// myArray.splice(2, 1);   // Menghapus dari index 2 sebanyak 1 elemen
+// console.log(myArray);
